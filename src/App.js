34d08f8c22db3d9f1358dcca112a9fcc66f400ae
saim1123 <saimtahir3211@gmail.com>
2024-01-08 17:46:56 +0500

    setting the routes with react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route index element={<h1>Hello World</h1>}></Route>
+          <Route path="movie/:id" element={<h1>Movie Detail Page</h1>}></Route>
+          <Route path="movies/:type" element={<h1>Movie Type</h1>}></Route>
+          <Route path="/*" element={<h1>Error</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
