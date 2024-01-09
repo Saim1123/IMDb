46d95fbbdb823e2b44d025e63c04c763187fbf0a
saim1123 <saimtahir3211@gmail.com>
2024-01-09 16:29:46 +0500

    build and style the card component and movieList components

---
+++ src/components/movieList/MovieList.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "./MovieList.css";
+import Card from "../card/Card";
+import { useParams } from "react-router-dom";
+
+const MovieList = () => {
+  const [movieList, setMovieList] = useState([]);
+  const { type } = useParams();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [type]);
+
+  const getData = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        type ? type : "popular"
+      }?api_key=04650dd1d369e12be79a5e4fe0c389c1&language=en-US`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMovieList(data.results);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div className="movie__list">
+      <h2>{(type ? type : "POPULAR").toUpperCase()}</h2>
+      <div className="list__cards">
+        {movieList.map((movie) => {
+          return <Card key={movie.id} movie={movie} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MovieList;
